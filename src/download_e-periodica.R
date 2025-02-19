32e90110e6ccb8bb6dbdd76b028bd63cb60867ac
mtwente <127845092+mtwente@users.noreply.github.com>
2025-02-19 14:23:39 +0100

    feat: rotate single page in faulty pdf file
    this is to address a scanning error in a single file on e-periodica.ch

--- src/download_e-periodica.R
+++ src/download_e-periodica.R
@@ -2,6 +2,7 @@ library(httr)
 library(readr)
 library(dplyr)
 library(here)
+library(qpdf)
 
 input_file <- here("docs", "articles_metadata.csv")
 output_folder <- here("data", "raw", "pdf")
@@ -55,3 +56,23 @@ articles <- articles %>% filter(!is.na(url))  # Remove rows with NA URLs
 apply(articles, 1, function(row) {
   download_pdf(row["url"], row["id"])
 })
+
+# Rotate faulty page in one specific file
+pdf_to_rotate <- file.path(output_folder, "sbz-002_1949_67__519_d.pdf")
+# Define a temporary output file
+temp_output <- file.path(output_folder, "sbz-002_1949_67__519_d_temp.pdf")
+
+if (file.exists(pdf_to_rotate)) {
+  pdf_rotate_pages(
+    input = pdf_to_rotate,
+    pages = 9,
+    angle = -180,
+    relative = TRUE,
+    output = temp_output
+  )
+  
+  # Overwrite original file
+  file.rename(temp_output, pdf_to_rotate)
+  
+  message("Rotated page 9 in ", basename(pdf_to_rotate))
+}
