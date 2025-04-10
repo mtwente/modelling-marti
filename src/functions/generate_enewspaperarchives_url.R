e45e04afd597b143868af4e5c05a367778c050b9
mtwente <127845092+mtwente@users.noreply.github.com>
2025-04-10 14:06:07 +0200

    fix: fix function call

--- src/functions/generate_enewspaperarchives_url.R
+++ src/functions/generate_enewspaperarchives_url.R
@@ -3,5 +3,5 @@
 library(tidyverse)
 
 generate_veridian_url <- function(article_id) {
-  glue("https://www.e-newspaperarchives.ch/?a=da&command=getSectionText&d={article_id}&srpos=&f=AJAX&e=-------de-20--1--img-txIN--------0-----")
+  str_glue("https://www.e-newspaperarchives.ch/?a=da&command=getSectionText&d={article_id}&srpos=&f=AJAX&e=-------de-20--1--img-txIN--------0-----")
 }
