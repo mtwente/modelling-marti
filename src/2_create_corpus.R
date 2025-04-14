47c34a394375d3120a8d02f77b820d3f26a22886
mtwente <127845092+mtwente@users.noreply.github.com>
2025-04-14 18:55:02 +0200

    feat: format date column in corpus

--- src/2_create_corpus.R
+++ src/2_create_corpus.R
@@ -1,5 +1,6 @@
 library(here)
 library(readtext)
+library(tidyverse)
 library(quanteda)
 
 # Load Metadata Creation Function
@@ -15,8 +16,27 @@ metadata <- read.csv(here("docs", "articles_metadata.csv"), sep = ";")
 text_data <- merge(text_data, metadata[, c("id", "title", "publication", "date", "lang")], 
                    by.x = "doc_id", by.y = "id", all.x = TRUE)
 
+# Convert Date Column to Actual Date Format
+text_data <- text_data %>%
+  mutate(
+    date = case_when(
+      # if the date is just a 4-digit year (e.g., "1960")
+      str_detect(date, "^\\d{4}$") ~ ymd(paste0(date, "-01-01")),
+      
+      # if the date is in %d.%m.%y format
+      str_detect(date, "^\\d{2}\\.\\d{2}\\.\\d{2}$") ~ {
+        parsed <- dmy(date)
+        # Ensure the year is treated as 1900s
+        update(parsed, year = if_else(year(parsed) > 1999, year(parsed) - 100, year(parsed)))
+      },
+      
+      TRUE ~ NA_Date_  # if no date is found
+    )
+  )
+
 # Keep German Texts Only
-text_data <- text_data[text_data$lang == "de", ]
+text_data <- text_data %>%
+  filter(lang == "de")
 
 # Create Corpus -------
 marti_corpus <- corpus(text_data, text_field = "text")
@@ -28,7 +48,7 @@ write.csv(corpus_df, here("build", "marti_corpus.csv"), row.names = FALSE)
 # Create Corpus Metadata File -------
 meta_marti <- annotate(data = corpus_df,
                       title = "Publizistische Tätigkeit von Hans Marti (Korpus)",
-                      column_description = c("ID des Artikels", "Volltext des Artikels, je nach Text generiert aus PDF-Dateien oder aus OCR-Transkription auf e-newspaperarchives.ch", "Titel des Artikels", "Publikation, in dem der Artikel (erst-)veröffentlicht wurde", "Veröffentlichungsdatum im Format %d.%m.%y oder, vereinzelt, im Format %Y.", "ISO 639-1:2002-Code der Sprache, in der der Artikel verfasst ist"),
+                      column_description = c("ID des Artikels", "Volltext des Artikels, je nach Text generiert aus PDF-Dateien oder aus OCR-Transkription auf e-newspaperarchives.ch", "Titel des Artikels", "Publikation, in dem der Artikel (erst-)veröffentlicht wurde", "Veröffentlichungsdatum im Format %Y-%m-%d. Für die laut Daten am 01.01. eines jeweiligen Jahres publizierten Beiträge sind in der Regel keine tagesgenauen Angaben vorhanden.", "ISO 639-1:2002-Code der Sprache, in der der Artikel verfasst ist"),
                       subject = c("Hans Marti", "Raumplanung", "Planungsgeschichte", "Schweizerische Bauzeitung", "Neue Zürcher Zeitung", "Das Werk", "Plan"),
                       object_description = "...",
                       creator = list(name = "Moritz Twente",
