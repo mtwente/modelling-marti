aeec88f8bd27bf3d421bfdc381e9d285808b7e07
mtwente <127845092+mtwente@users.noreply.github.com>
2025-04-14 14:52:00 +0200

    feat: create corpus

--- src/2_create_corpus.R
+++ src/2_create_corpus.R
@@ -1,68 +1,42 @@
-library(readtext)
 library(here)
+library(readtext)
 library(quanteda)
-library(quanteda.textplots)
-library(seededlda)
 
+# Load Metadata Creation Function
+source(here("src", "functions", "annotate_metadata.R"))
+
+# Read Data -------
 text_data <- readtext(here("data", "clean", "*.txt"))
 text_data$doc_id <- sub("\\.txt$", "", text_data$doc_id)
 
 metadata <- read.csv(here("docs", "articles_metadata.csv"), sep = ";")
 
-# merge .txt data with metadata from overview file
+# Merge .txt Data with Metadata
 text_data <- merge(text_data, metadata[, c("id", "title", "publication", "date", "lang")], 
                    by.x = "doc_id", by.y = "id", all.x = TRUE)
 
-# remove articles that are not in German
+# Keep German Texts Only
 text_data <- text_data[text_data$lang == "de", ]
 
-# create corpus
+# Create Corpus -------
 marti_corpus <- corpus(text_data, text_field = "text")
-summary(marti_corpus)
 
-# tokenize corpus
-marti_tokens <- tokens(marti_corpus,
-                       remove_punct = TRUE,
-                       remove_numbers = TRUE,
-                       remove_symbols = TRUE) %>%
-  tokens_tolower()
+# Write Corpus to CSV
+corpus_df <- convert(marti_corpus, to = "data.frame")
+write.csv(corpus_df, here("build", "marti_corpus.csv"), row.names = FALSE)
 
-summary(marti_tokens, 5)
-
-# explore corpus
-kwic(marti_tokens, pattern = "Locle")
-
-head(docvars(marti_corpus))
-
-# remove stopwords
-marti_tokens_nostop <- tokens_select(marti_tokens,
-                                     pattern = c(stopwords("de"), "dass"),
-                                     selection = "remove")
-
-# create dfm
-dfmat_marti <- marti_corpus |>
-  tokens(remove_punct = TRUE,
-         remove_numbers = TRUE,
-         remove_symbols = TRUE) |>
-  tokens_select(pattern = c(stopwords("de"), "dass"),
-                selection = "remove") |>
-  tokens_tolower() |>
-  dfm()
-
-dfmat_marti_clean <- dfm_select(dfmat_marti, min_nchar = 2)
-
-print(dfmat_marti_clean)
-
-dfmat_marti_stem <- dfm_wordstem(dfmat_marti_clean, language = "de")
-print(dfmat_marti_stem)
-
-textplot_wordcloud(dfmat_marti_clean)
-topfeatures(dfmat_marti_clean, 20)
-
-textplot_wordcloud(dfmat_marti_stem)
-topfeatures(dfmat_marti_stem, 20)
-
-# topic modelling
-tmod_lda <- textmodel_lda(dfmat_marti_stem, k = 10)
-terms(tmod_lda, 10)
-topics(tmod_lda)
+# Create Corpus Metadata File -------
+meta_marti <- annotate(data = corpus_df,
+                      title = "Publizistische Tätigkeit von Hans Marti (Korpus)",
+                      column_description = c("ID des Artikels", "Volltext des Artikels, je nach Text generiert aus PDF-Dateien oder aus OCR-Transkription auf e-newspaperarchives.ch", "Titel des Artikels", "Publikation, in dem der Artikel (erst-)veröffentlicht wurde", "Veröffentlichungsdatum im Format %d.%m.%y oder, vereinzelt, im Format %Y.", "ISO 639-1:2002-Code der Sprache, in der der Artikel verfasst ist"),
+                      subject = c("Hans Marti", "Raumplanung", "Planungsgeschichte", "Schweizerische Bauzeitung", "Neue Zürcher Zeitung", "Das Werk", "Plan"),
+                      object_description = "...",
+                      creator = list(name = "Moritz Twente",
+                                     email = "mtwente@protonmail.com",
+                                     orcid = "0009-0005-7187-9774"),
+                      date = "1946/1989",
+                      temporal = "Contemporary",
+                      source = "Volltexte von e-periodica.ch und e-newspaperarchives.ch. Bibliographie nach Ruedin/Hanak 2008 und eigener Recherche. Ersteller: Moritz Twente",
+                      rights = "",
+                      license = ""
+)
