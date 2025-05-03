8d62bbf0b2b6cd53987fa6f18ffb5bac0fcd8cff
mtwente <127845092+mtwente@users.noreply.github.com>
2025-05-04 00:41:47 +0200

    feat: expand preprocessing workflow

--- src/3_preprocessing.R
+++ src/3_preprocessing.R
@@ -7,6 +7,8 @@ library(quanteda.textplots)
 library(quanteda.textstats)
 library(RColorBrewer)
 library(dplyr)
+#library(ggplot2)
+library(dendextend)
 
 # read corpus
 text_data <- read_csv(here("build", "marti_corpus.csv"),
@@ -29,7 +31,7 @@ text_data %>% group_by(publication) %>% summarise(Count = n())
 
 ## tokenize & remove stopwords
 
-marti_tokens <- marti_corpus %>%
+marti_tokens_full <- marti_corpus %>%
   tokens(remove_punct = TRUE,
          remove_numbers = TRUE,
          remove_symbols = TRUE) %>%
@@ -39,28 +41,66 @@ extra_stopwords <- c("ja", "dass", "müssen", "schon", "wäre", "würde", "worde
                      "wurde", "wurden", "sollen", "a", "h", "dr", "i", "s", "beim",
                      "sei", "überhaupt", "gerade", "einfach", "nämlich", "wer", "dafür", "m")
 
-dfm_marti <- marti_tokens %>%
+marti_tokens_clean <- marti_tokens_full %>%
   tokens_remove(pattern = c(stopwords("de"),
-                            extra_stopwords)) %>%
-  dfm()
+                            extra_stopwords))
+
+#dfm_marti <- dfm(marti_tokens_clean)
 
 # Exploration
+#topfeatures(dfm_marti)
 
+# find collocations
+
+marti_tokens_collocations <- textstat_collocations(marti_tokens_clean,
+                                                   min_count = 3)
+head(marti_tokens_collocations, 10)
+
+# nur kanton ZH und Region ZH und Agglomeration ZH verwenden, aber nicht stadt
+collocations <- c("kanton zürich", "agglomeration zürich", "region zürich",
+                  "kanton solothurn", "kanton bern", "kanton st gallen", "st gallen",
+                  "hans marti", "max frisch", "werner moser", "rolf meyer",
+                  "vereinigung landesplanung") %>%
+  phrase()
+
+marti_tokens_compounded <- tokens_compound(marti_tokens_clean,
+                                           pattern = collocations)
+
+kw_comp <- kwic(marti_tokens_compounded, pattern = c("kanton_st_gallen"))
+
+dfm_marti <- dfm(marti_tokens_compounded)
 topfeatures(dfm_marti)
 
-# remove sparse terms
-dfm_trimmed <- dfm_trim(dfmat_marti, min_docfreq = 2)
-topfeatures(dfm_trimmed)
+# Sparse Terms
 
-textplot_wordcloud(dfm_trimmed, scale = c(3.5, 0.75), colors = brewer.pal(8, "Dark2"), random.order = F, 
+## Explore sparse terms
+dfm_sparse <- dfm_trim(dfm_marti, max_docfreq = 2)
+topfeatures(dfm_sparse)
+
+# Remove sparse terms
+dfm_marti <- dfm_trim(dfm_marti, min_docfreq = 2)
+
+textplot_wordcloud(dfm_marti, scale = c(3.5, 0.75), colors = brewer.pal(8, "Dark2"), random.order = F, 
                    rot.per = 0.1, max.words = 100)
 
 #dfmat_group <- dfm_group(dfm_trimmed, dfm_trimmed$publication)
 
 #cloud_comparison <- textplot_wordcloud(dfmat_group, comparison = TRUE, max_words = 100, colors = brewer.pal(8, "Dark2"))
   
-wordDfm <- dfm_sort(dfm_weight(dfmat_marti, "prop"))
-wordDfm <- t(wordDfm)[1:50, ]  # because transposed
+wordDfm <- dfm_sort(dfm_weight(dfm_marti, "prop"))
+wordDfm <- t(wordDfm)[1:50, ]
 wordDistMat <- dist(wordDfm)
 wordCluster <- hclust(wordDistMat)
-plot(wordCluster, xlab = "", main = "Proportional weighting")
+
+dend <- as.dendrogram(wordCluster) %>%
+  hang.dendrogram()
+#dend <- hang.dendrogram(dend)
+
+# Optional customizations
+dend <- set(dend, "labels_cex", 0.5)
+dend <- set(dend, "branches_lwd", 1.2)
+
+# Plot with horizontal layout and rotated labels
+plot_horiz.dendrogram(dend,
+                      side = F,
+                      )
