0239732f69393398366429763a375479ca75188c
mtwente <127845092+mtwente@users.noreply.github.com>
2025-05-04 14:27:04 +0200

    feat: add explorative visualization plots

--- src/3_preprocessing.R
+++ src/3_preprocessing.R
@@ -7,7 +7,7 @@ library(quanteda.textplots)
 library(quanteda.textstats)
 library(RColorBrewer)
 library(dplyr)
-#library(ggplot2)
+library(stm)
 library(dendextend)
 
 # read corpus
@@ -39,16 +39,12 @@ marti_tokens_full <- marti_corpus %>%
 
 extra_stopwords <- c("ja", "dass", "müssen", "schon", "wäre", "würde", "worden",
                      "wurde", "wurden", "sollen", "a", "h", "dr", "i", "s", "beim",
-                     "sei", "überhaupt", "gerade", "einfach", "nämlich", "wer", "dafür", "m")
+                     "sei", "überhaupt", "gerade", "einfach", "nämlich", "wer", "dafür", "m",
+                     "gar", "ganz", "wären")
 
 marti_tokens_clean <- marti_tokens_full %>%
   tokens_remove(pattern = c(stopwords("de"),
                             extra_stopwords))
-
-#dfm_marti <- dfm(marti_tokens_clean)
-
-# Exploration
-#topfeatures(dfm_marti)
 
 # find collocations
 
@@ -60,13 +56,13 @@ head(marti_tokens_collocations, 10)
 collocations <- c("kanton zürich", "agglomeration zürich", "region zürich",
                   "kanton solothurn", "kanton bern", "kanton st gallen", "st gallen",
                   "hans marti", "max frisch", "werner moser", "rolf meyer",
-                  "vereinigung landesplanung") %>%
+                  "vereinigung landesplanung", "knonauer amt", "st margrethen") %>%
   phrase()
 
 marti_tokens_compounded <- tokens_compound(marti_tokens_clean,
                                            pattern = collocations)
 
-kw_comp <- kwic(marti_tokens_compounded, pattern = c("kanton_st_gallen"))
+kw_comp <- kwic(marti_tokens_compounded, pattern = c("knonauer*"))
 
 dfm_marti <- dfm(marti_tokens_compounded)
 topfeatures(dfm_marti)
@@ -77,6 +73,12 @@ topfeatures(dfm_marti)
 dfm_sparse <- dfm_trim(dfm_marti, max_docfreq = 2)
 topfeatures(dfm_sparse)
 
+## Explore Sparsity Settings with stm
+
+stmdfm <- convert(dfm_marti, to = "stm", docvars = docvars(marti_corpus))
+
+plotRemoved(stmdfm$documents, lower.thresh = seq(1, 10, by = 1))
+
 # Remove sparse terms
 dfm_marti <- dfm_trim(dfm_marti, min_docfreq = 2)
 
@@ -86,7 +88,8 @@ textplot_wordcloud(dfm_marti, scale = c(3.5, 0.75), colors = brewer.pal(8, "Dark
 #dfmat_group <- dfm_group(dfm_trimmed, dfm_trimmed$publication)
 
 #cloud_comparison <- textplot_wordcloud(dfmat_group, comparison = TRUE, max_words = 100, colors = brewer.pal(8, "Dark2"))
-  
+
+# Dendrogram
 wordDfm <- dfm_sort(dfm_weight(dfm_marti, "prop"))
 wordDfm <- t(wordDfm)[1:50, ]
 wordDistMat <- dist(wordDfm)
@@ -94,7 +97,6 @@ wordCluster <- hclust(wordDistMat)
 
 dend <- as.dendrogram(wordCluster) %>%
   hang.dendrogram()
-#dend <- hang.dendrogram(dend)
 
 # Optional customizations
 dend <- set(dend, "labels_cex", 0.5)
@@ -102,5 +104,19 @@ dend <- set(dend, "branches_lwd", 1.2)
 
 # Plot with horizontal layout and rotated labels
 plot_horiz.dendrogram(dend,
-                      side = F,
-                      )
+                      side = F)
+
+# Co-Occurrence Network
+topterms <- names(topfeatures(dfm_marti, 30))
+
+fcmat_marti_topterms <- fcm(dfm_marti) %>%
+  fcm_select(pattern = topterms)
+
+prop_size <- rowSums(fcmat_marti_topterms)/min(rowSums(fcmat_marti_topterms))
+
+set.seed(20)
+textplot_network(fcmat_marti_topterms,
+                 min_freq = 0.5,
+                 edge_alpha = 0.5, 
+                 edge_size = 1.3,
+                 vertex_labelsize = 0.7*prop_size)
